0c2ee1e30dafe1dacc115ee112f242c18cae7358
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-19 15:44:23 +0800

    add autopairs

--- lua/becauselol/plugins/telescope.lua
+++ lua/becauselol/plugins/telescope.lua
@@ -3,10 +3,11 @@ return {
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy find files in cwd" })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   end
     }
