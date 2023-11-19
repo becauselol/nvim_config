cdd1bfad8b07ea9a868ae876506fb94a1327672d
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-19 15:10:51 +0800

    committing initial plugins

---
+++ init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+--require("becauselol.core")
+require("becauselol.lazy")
