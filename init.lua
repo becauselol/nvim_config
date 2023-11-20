49c7d46fdfddcfd99f57e71ea6cc16f1e5fc67f3
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-20 14:06:19 +0800

    feat: add toggleterm

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.wo.number = true
 vim.wo.relativenumber = true
