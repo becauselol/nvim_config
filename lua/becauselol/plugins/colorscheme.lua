b1f4081a306a06b89305f0df43aa03f5bf4712d3
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-21 15:45:14 +0800

    chore: tried some colorschemes but onedark is dope

--- lua/becauselol/plugins/colorscheme.lua
+++ lua/becauselol/plugins/colorscheme.lua
@@ -1,3 +1,53 @@
+--return {
+--		"catppuccin/nvim",
+--		name = "catppuccin",
+--		opts = {
+--			term_colors = true,
+--			transparent_background = false,
+--			styles = {
+--				comments = {},
+--				conditionals = {},
+--				loops = {},
+--				functions = {},
+--				keywords = {},
+--				strings = {},
+--				variables = {},
+--				numbers = {},
+--				booleans = {},
+--				properties = {},
+--				types = {},
+--			},
+--			color_overrides = {
+--				mocha = {
+--					base = "#000000",
+--					mantle = "#000000",
+--					crust = "#000000",
+--				},
+--			},
+--			integrations = {
+--				telescope = {
+--					enabled = true,
+--					style = "nvchad",
+--				},
+--				dropbar = {
+--					enabled = true,
+--					color_mode = true,
+--				},
+--			},
+--		},
+--}
+--return {
+--    "tiagovla/tokyodark.nvim",
+--    opts = {
+--        -- custom options here
+--        transparent_background = true
+--    },
+--    config = function(_, opts)
+--        require("tokyodark").setup(opts) -- calling setup is optional
+--        vim.cmd [[colorscheme tokyodark]]
+--    end,
+--}
+
 return {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
