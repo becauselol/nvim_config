4b18f09cf72f5e5553dd085a25e501ac0cacdbd2
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-21 16:27:13 +0800

    feat: kanagawa final

--- lua/becauselol/plugins/colorscheme.lua
+++ lua/becauselol/plugins/colorscheme.lua
@@ -1,3 +1,15 @@
+return {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+        --theme = "dragon"
+    },
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme kanagawa-dragon]])
+    end
+}
 --return {
 --		"catppuccin/nvim",
 --		name = "catppuccin",
@@ -48,17 +60,17 @@
 --    end,
 --}
 
-return {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    opts = {
-        style = "darker",
-    },
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-}
+--return {
+--    -- Theme inspired by Atom
+--    'navarasu/onedark.nvim',
+--    priority = 1000,
+--    opts = {
+--        style = "darker",
+--    },
+--    config = function()
+--      vim.cmd.colorscheme 'onedark'
+--    end,
+--}
 --return {
 --    "folke/tokyonight.nvim",
 --    lazy = false, -- make sure we load this during startup if it is your main colorscheme
