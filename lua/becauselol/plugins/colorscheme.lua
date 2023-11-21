90630c9e1f8d8bbabeebfeb5502bfa66371f8fc5
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-21 15:24:30 +0800

    feat: change colorscheme to onedark darker

--- lua/becauselol/plugins/colorscheme.lua
+++ lua/becauselol/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    opts = {
+        style = "darker",
+    },
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme 'onedark'
     end,
-  }
+}
+--return {
+--    "folke/tokyonight.nvim",
+--    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--    priority = 1000, -- make sure to load this before all the other start plugins
+--    config = function()
+--      -- load the colorscheme here
+--      vim.cmd([[colorscheme tokyonight]])
+--    end,
+--  }
